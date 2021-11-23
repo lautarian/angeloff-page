27ee59ca9153b05d106db61cb9e7647f3a150b29
cesar <cesar.angeloff@nortedesarrollos.com>
2021-11-23 16:46:58 -0300

    .

--- src/customCard.js
+++ src/customCard.js
@@ -21,7 +21,7 @@ export default function CustomCard({ propiedad,visibilizar }) {
         justifyContent="center"
         style={{ width: "20%" }}
       >
-
+        
         <img class="img-fluid" src={propiedad.imagen} width="50px" height="50px"/>
 
         {" "}
